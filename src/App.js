8dc80181d72816ba4966a07ae0b80ac282d8a505
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2018-10-15 00:20:22 -0700

    change data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Cristian 2
+            Hi Cristian 3
           </p>
           <a
             className="App-link"
